fb0a6910c2de4964489afba961ca3b5c3a0602ff
Manuel Neto <up202108744@fe.up.pt>
2023-03-06 22:47:57 +0000

    added documentation

--- lab2/i8254.h
+++ lab2/i8254.h
@@ -6,12 +6,12 @@
 /** @defgroup i8254 i8254
  * @{
  *
- * Constants for programming the i8254 Timer. Needs to be completed.
+ * Constants for programming the i8254 Timer.
  */
 
 #define TIMER_FREQ 1193182 /**< @brief clock frequency for timer in PC and AT */
-#define TIMER0_IRQ 0 /**< @brief Timer 0 IRQ line */
-#define TIMER_MIN_FREQ 19
+#define TIMER0_IRQ 0       /**< @brief Timer 0 IRQ line */
+#define TIMER_MIN_FREQ 19  /**< @brief Timer lowest frequency possible */
 
 /* I/O port addresses */
 
@@ -20,7 +20,7 @@
 #define TIMER_2    0x42 /**< @brief Timer 2 count register */
 #define TIMER_CTRL 0x43 /**< @brief Control register */
 
-#define TIMER_PORT(timer) TIMER_0 + timer
+#define TIMER_PORT(timer) TIMER_0 + timer /**< @brief Timer count register */
 
 #define SPEAKER_CTRL 0x61 /**< @brief Register for speaker control  */
 
@@ -33,7 +33,7 @@
 #define TIMER_SEL2   BIT(7)            /**< @brief Control Word for Timer 2 */
 #define TIMER_RB_CMD (BIT(7) | BIT(6)) /**< @brief Read Back Command */
 
-#define TIMER_SEL(timer) (timer << 6)
+#define TIMER_SEL(timer) (timer << 6)  /**< @brief Control Word for Timer */
 
 /* Register selection: bits 5 and 4 */
 
@@ -53,13 +53,13 @@
 
 /* READ-BACK COMMAND FORMAT */
 
-#define TIMER_RB_COUNT_  BIT(5)
-#define TIMER_RB_STATUS_ BIT(4)
-#define TIMER_RB_SEL(n)  BIT((n) + 1)
+#define TIMER_RB_COUNT_  BIT(5)       /**< @brief Read-back timer programmed mode */
+#define TIMER_RB_STATUS_ BIT(4)       /**< @brief Read-back timer counter value */
+#define TIMER_RB_SEL(n)  BIT((n) + 1) /**< @brief Read-back select timer */
 
-#define TIMER_IN_MODE (BIT(5) | BIT(4))
-#define TIMER_COUNT_MODE (BIT(3) | BIT(2) | BIT(1))
-#define TIMER_COUNT_MODE_DONT_CARE (BIT (1) | BIT(0))
+#define TIMER_IN_MODE (BIT(5) | BIT(4))               /**< @brief Bitmask for timer counter inicialization */
+#define TIMER_COUNT_MODE (BIT(3) | BIT(2) | BIT(1))   /**< @brief Bitmask for timer counting mode */
+#define TIMER_COUNT_MODE_DONT_CARE (BIT (1) | BIT(0)) /**< @brief Bitmask to reset timer counting mode don't care bits*/
 
 /**@}*/
 
