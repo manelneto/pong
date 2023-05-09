f879682901f8091b3b28784bdc5fbaf9be089c2a
Manuel Neto <up202108744@fe.up.pt>
2023-05-09 16:14:47 +0100

    added menu file with states enum only

---
+++ proj/src/game/menu.h
@@ -0,0 +1,21 @@
+#ifndef _MENU_H_
+#define _MENU_H_
+
+/** @defgroup menu menu
+ * @{
+ * 
+ * Functions related to the menu
+*/
+
+/**
+ * @brief Enum for the menu states
+*/
+typedef enum {
+    START, /*!< start game */
+    GAME,  /*!< in game */
+    END    /*!< end game */
+} MenuState;
+
+/**@}*/
+
+#endif /* _MENU_H_ */
