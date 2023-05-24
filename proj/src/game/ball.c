0f89ca3541883620e14882d4e3f0fbf68b71abc0
Manuel Neto <up202108744@fe.up.pt>
2023-05-24 17:42:44 +0100

    minor changes to ball: types of constructor parameters and sanity checks

--- proj/src/game/ball.c
+++ proj/src/game/ball.c
@@ -4,7 +4,7 @@
 
 #include "../drivers/video_gr.h"
 
-Ball *construct_ball(int16_t x, int16_t y, int16_t vx, int16_t vy) {
+Ball *construct_ball(uint16_t x, uint16_t y, int8_t vx, int8_t vy) {
   Ball *ball = (Ball *) malloc(sizeof(Ball));
 
   if (!ball) {
@@ -21,6 +21,11 @@ Ball *construct_ball(int16_t x, int16_t y, int16_t vx, int16_t vy) {
 }
 
 int draw_ball(Ball *ball) {
+  if (!ball) {
+    printf("%s: ball is NULL\n", __func__);
+    return 1;
+  }
+
   if (vg_draw_pixel(ball->x, ball->y, 0xffffff)) {
     printf("%s: vg_draw_pixel(ball->x: %d, ball->y: %d, color: 0x%x) error\n", __func__, ball->x, ball->y, 0xffffff);
     return 1;
@@ -29,17 +34,32 @@ int draw_ball(Ball *ball) {
   return 0;
 }
 
-void move_ball(Ball *ball) {
+int move_ball(Ball *ball) {
+  if (!ball) {
+    printf("%s: ball is NULL\n", __func__);
+    return 1;
+  }
+
   ball->x += ball->vx;
   ball->y += ball->vy;
 
-  if (ball->y <= 0 || ball->y >= vmi_p.YResolution) {
+  if (ball->y < 0) {
     ball->vy = -ball->vy;
+    ball->y = 0;
+  } else if (ball->y >= vmi_p.YResolution) {
+    ball->vy = -ball->vy;
+    ball->y = vmi_p.YResolution - 1;
   }
 
-  if (ball->x <= 0 || ball->x >= vmi_p.XResolution) {
+  if (ball->x < 0) {
     ball->vx = -ball->vx;
+    ball->x = 0;
+  } else if (ball->x >= vmi_p.XResolution) {
+    ball->vx = -ball->vx;
+    ball->x = vmi_p.XResolution - 1;
   }
+
+  return 0;
 }
 
 void destroy_ball(Ball *ball) {
--- proj/src/game/ball.h
+++ proj/src/game/ball.h
@@ -13,10 +13,10 @@
  * @brief Struct for the ball
 */
 typedef struct {
-    int16_t x; /*!< horizontal coordinate of the ball */
-    int16_t y; /*!< vertical coordinate of the ball */
-    int16_t vx; /*!< horizontal velocity of the ball */
-    int16_t vy; /*!< vertical velocity of the ball */
+    uint16_t x; /*!< horizontal coordinate of the ball */
+    uint16_t y; /*!< vertical coordinate of the ball */
+    int8_t vx;  /*!< horizontal velocity of the ball */
+    int8_t vy;  /*!< vertical velocity of the ball */
 } Ball;
 
 /**
@@ -28,12 +28,13 @@ typedef struct {
  * @param vy vertical velocity of the ball
  * @return Constructed ball on success, NULL otherwise
 */
-Ball* construct_ball(int16_t x, int16_t y, int16_t vx, int16_t vy);
+Ball* construct_ball(uint16_t x, uint16_t y, int8_t vx, int8_t vy);
 
 /**
  * @brief Ball drawer
  * 
  * @param ball pointer to the ball to be drawn
+ * @return Return 0 upon success and non-zero otherwise
 */
 int draw_ball(Ball *ball);
 
@@ -41,8 +42,9 @@ int draw_ball(Ball *ball);
  * @brief Updates ball position and velocity
  * 
  * @param ball pointer to the ball to be updated/moved
+ * @return Return 0 upon success and non-zero otherwise
 */
-void move_ball(Ball *ball);
+int move_ball(Ball *ball);
 
 /**
  * @brief Ball destructor
