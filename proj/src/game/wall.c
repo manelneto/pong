38486aa451d10056a447d75fb2f8d7899460ae46
Manuel Neto <up202108744@fe.up.pt>
2023-05-24 17:47:45 +0100

    minor changes to wall: types of constructor parameters and sanity checks

--- proj/src/game/wall.c
+++ proj/src/game/wall.c
@@ -4,7 +4,7 @@
 
 #include "../drivers/video_gr.h"
 
-Wall *construct_wall(int16_t x, int16_t y, uint16_t l) {
+Wall *construct_wall(int16_t x, int16_t y, uint8_t l) {
   Wall *wall = (Wall *) malloc(sizeof(Wall));
 
   if (!wall) {
@@ -15,11 +15,17 @@ Wall *construct_wall(int16_t x, int16_t y, uint16_t l) {
   wall->x = x;
   wall->y = y;
   wall->l = l;
+  wall->direction = STOP;
 
   return wall;
 }
 
 int draw_wall(Wall *wall) {
+  if (!wall) {
+    printf("%s: wall is NULL\n", __func__);
+    return 1;
+  }
+
   for (int16_t i = 0; i < wall->l; i++)
     if (vg_draw_pixel(wall->x, wall->y + i, 0xffffff)) {
       printf("%s: vg_draw_pixel(wall->x: %d, wall->y + i: %d, color: 0x%x) error\n", __func__, wall->x, wall->y + i, 0xffffff);
@@ -29,16 +35,25 @@ int draw_wall(Wall *wall) {
   return 0;
 }
 
-void move_wall(Wall *wall, Direction direction) {
-  if (direction == UP)
+int move_wall(Wall *wall) {
+  if (!wall) {
+    printf("%s: wall is NULL\n", __func__);
+    return 1;
+  }
+
+  if (wall->direction == UP)
     wall->y -= wall->l/5;
-  else if (direction == DOWN)
+  else if (wall->direction == DOWN)
     wall->y += wall->l/5;
 
   if (wall->y < 0)
     wall->y = 0;
   else if (wall->y + wall->l >= vmi_p.YResolution)
     wall->y = vmi_p.YResolution - wall->l - 1;
+  
+  wall->direction = STOP;
+
+  return 0;
 }
 
 void destroy_wall(Wall *wall) {
--- proj/src/game/wall.h
+++ proj/src/game/wall.h
@@ -22,26 +22,27 @@ typedef enum {
  * @brief Struct for the wall
 */
 typedef struct {
-    int16_t x; /*!< horizontal coordinate of the wall */
-    int16_t y; /*!< initial vertical coordinate of the wall */
-    int16_t l; /*!< length of the wall */
+    int16_t x;           /*!< horizontal coordinate of the wall */
+    int16_t y;           /*!< initial vertical coordinate of the wall */
+    uint8_t l;           /*!< length of the wall */
+    Direction direction; /*!< direction of the wall movement */
 } Wall;
 
 /**
- * @brief wall constructor
+ * @brief Wall constructor
  * 
  * @param x horizontal coordinate of the wall
  * @param y initial vertical coordinate of the wall
  * @param l length of the wall
  * @return Constructed wall on success, NULL otherwise
 */
-Wall* construct_wall(int16_t x, int16_t y, uint16_t l);
+Wall* construct_wall(int16_t x, int16_t y, uint8_t l);
 
 /**
  * @brief Wall drawer
  * 
  * @param wall pointer to the wall to be drawn
- * 
+ * @return Return 0 upon success and non-zero otherwise
 */
 int draw_wall(Wall *wall);
 
@@ -49,9 +50,9 @@ int draw_wall(Wall *wall);
  * @brief Moves wall up/down according to direction
  * 
  * @param wall pointer to the wall to be moved
- * @param direction direction to move the wall
+ * @return Return 0 upon success and non-zero otherwise
 */
-void move_wall(Wall *wall, Direction direction);
+int move_wall(Wall *wall);
 
 /**
  * @brief Wall destructor
