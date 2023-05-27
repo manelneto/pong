c1479ef2cf84e3effb9055576bff0351df5eca57
Manuel Neto <up202108744@fe.up.pt>
2023-05-28 00:01:41 +0100

    reviewed, documented and formatted KBC

--- proj/src/controller/kbc.c
+++ proj/src/controller/kbc.c
@@ -2,99 +2,95 @@
 
 #include "kbc.h"
 
-int (kbc_read_status)(uint8_t *status) {
-	if (util_sys_inb(KBC_STATUS_REG, status)) {
-		printf("%s: util_sys_inb(KBC_STATUS_REG, status: 0x%x) error\n", __func__, *status);
-		return 1;
-	}
+int(kbc_read_status)(uint8_t *status) {
+  if (util_sys_inb(KBC_STATUS_REG, status)) {
+    printf("%s: util_sys_inb(KBC_STATUS_REG: 0x%x, status: 0x%x) error\n", __func__, KBC_STATUS_REG, *status);
+    return 1;
+  }
   return 0;
 }
 
-int (kbc_read_output)(uint8_t *output) {
-	uint8_t status;
-	for (int i = 0; i < KBC_MAX_ATTEMPTS; i++) {
-		if (kbc_read_status(&status)) {
-			printf("%s: kbc_read_status(status: 0x%x) error\n", __func__, status);
-			return 1;
-		}
+int(kbc_read_output)(uint8_t *output) {
+  uint8_t status;
+  for (int i = 0; i < KBC_MAX_ATTEMPTS; i++) {
+    if (kbc_read_status(&status)) {
+      printf("%s: kbc_read_status(status: 0x%x) error\n", __func__, status);
+      return 1;
+    }
 
-		/* loop while 8042 output buffer is empty */
+    /* loop while 8042 output buffer is empty */
 
-		if (status & KBC_OBF) {
-			if (util_sys_inb(KBC_OUT_BUF, output)) {
-				printf("%s: util_sys_inb(KBC_OUT_BUF, 0x%x) error\n", __func__, *output);
-				return 1;
-			}
+    if (status & KBC_OBF) {
+      if (util_sys_inb(KBC_OUT_BUF, output)) {
+        printf("%s: util_sys_inb(KBC_OUT_BUF: 0x%x, 0x%x) error\n", __func__, KBC_OUT_BUF, *output);
+        return 1;
+      }
 
-			if ((status & (KBC_PARITY_ERROR | KBC_TIMEOUT_ERROR)) == 0)
-				return 0;
-			
-			return -1;
-		}
-		/*if (tickdelay(micros_to_ticks(KBD_DELAY))) {
-			printf("%s: tickdelay(micros_to_ticks(KBD_DELAY)) error\n", __func__);
-			return 1;
-		}*/
-	}
-	return 2;
+      if ((status & (KBC_PARITY_ERROR | KBC_TIMEOUT_ERROR)) == 0)
+        return 0;
+
+      return -1;
+    }
+  }
+  return 2;
 }
 
-int (kbc_read_acknowledgment_byte)(uint8_t *acknowledgment_byte) {
-	if (util_sys_inb(KBC_OUT_BUF, acknowledgment_byte)) {
-		printf("%s: util_sys_inb(KBC_OUT_BUF, acknowledgment_byte: 0x%x) error\n", __func__, *acknowledgment_byte);
-		return 1;
-	}
-	return 0;
+int(kbc_read_acknowledgment_byte)(uint8_t *acknowledgment_byte) {
+  if (util_sys_inb(KBC_OUT_BUF, acknowledgment_byte)) {
+    printf("%s: util_sys_inb(KBC_OUT_BUF: 0x%x, acknowledgment_byte: 0x%x) error\n", __func__, KBC_OUT_BUF, *acknowledgment_byte);
+    return 1;
+  }
+  return 0;
 }
 
-int (kbc_write_command)(uint8_t command) {
-	uint8_t status;
-	for (int i = 0; i < KBC_MAX_ATTEMPTS; i++) {
-		if (kbc_read_status(&status)) {
-			printf("%s: kbc_read_status(status: 0x%x) error\n", __func__, status);
-			return 1;
-		}
+int(kbc_write_command)(uint8_t command) {
+  uint8_t status;
+  for (int i = 0; i < KBC_MAX_ATTEMPTS; i++) {
+    if (kbc_read_status(&status)) {
+      printf("%s: kbc_read_status(status: 0x%x) error\n", __func__, status);
+      return 1;
+    }
 
-		/* loop while 8042 input buffer is not empty */
+    /* loop while 8042 input buffer is not empty */
 
-		if ((status & KBC_IBF) == 0) {
-			if (sys_outb(KBC_CMD_REG, command)) {
-				printf("%s: sys_outb(KBC_CMD_REG, command: 0x%x) error\n", __func__, command);
-				return 1;
-			}
-			return 0;
-		}
+    if ((status & KBC_IBF) == 0) {
+      if (sys_outb(KBC_CMD_REG, command)) {
+        printf("%s: sys_outb(KBC_CMD_REG: 0x%x, command: 0x%x) error\n", __func__, KBC_CMD_REG, command);
+        return 1;
+      }
+      return 0;
+    }
 
-		if (tickdelay(micros_to_ticks(KBD_DELAY))) {
-			printf("%s: tickdelay(micros_to_ticks(KBD_DELAY)) error\n", __func__);
-			return 1;
-		}
-	}
-	return 2;
+    if (tickdelay(micros_to_ticks(KBD_DELAY))) {
+      printf("%s: tickdelay(micros_to_ticks(KBD_DELAY: %d)) error\n", __func__, KBD_DELAY);
+      return 1;
+    }
+  }
+  return 2;
 }
 
-int (kbc_write_argument)(uint8_t argument) {
-	uint8_t status;
-	for (int i = 0; i < KBC_MAX_ATTEMPTS; i++) {
-		if (kbc_read_status(&status)) {
-			printf("%s: kbc_read_status(0x%x) error\n", __func__, status);
-			return 1;
-		}
+int(kbc_write_argument)(uint8_t argument) {
+  uint8_t status;
+  for (int i = 0; i < KBC_MAX_ATTEMPTS; i++) {
+    if (kbc_read_status(&status)) {
+      printf("%s: kbc_read_status(0x%x) error\n", __func__, status);
+      return 1;
+    }
 
-		/* loop while 8042 input buffer is not empty */
+    /* loop while 8042 input buffer is not empty */
 
-		if ((status & KBC_IBF) == 0) {
-			if (sys_outb(KBC_ARG_REG, argument)) {
-				printf("%s: sys_outb(KBC_ARG_REG, argument: 0x%x) error\n", __func__, argument);
-				return 1;
-			}
-			return 0;
-		}
+    if ((status & KBC_IBF) == 0) {
+      if (sys_outb(KBC_ARG_REG, argument)) {
+        printf("%s: sys_outb(KBC_ARG_REG: 0x%x, argument: 0x%x) error\n", __func__, KBC_ARG_REG, argument);
+        return 1;
+      }
+      return 0;
+    }
 
-		if (tickdelay(micros_to_ticks(KBD_DELAY))) {
-			printf("%s: tickdelay(micros_to_ticks(KBD_DELAY)) error\n", __func__);
-			return 1;
-		}
-	}
-	return 2;
+    if (tickdelay(micros_to_ticks(KBD_DELAY))) {
+      printf("%s: tickdelay(micros_to_ticks(KBD_DELAY: %d)) error\n", KBD_DELAY, __func__);
+      return 1;
+    }
+  }
+  return 2;
 }
--- proj/src/controller/kbc.h
+++ proj/src/controller/kbc.h
@@ -14,43 +14,43 @@
 
 /**
  * @brief Reads the KBC status
- * 
+ *
  * @param status address of memory position to be filled with the KBC status
  * @return Return 0 upon success and non-zero otherwise
-*/
-int (kbc_read_status)(uint8_t *status);
+ */
+int(kbc_read_status)(uint8_t *status);
 
 /**
  * @brief Reads the KBC output
- * 
+ *
  * @param output address of memory position to be filled with the KBC output
  * @return Return 0 upon success and non-zero otherwise (-1 if invalid data, 1 if internal error, 2 if exceeded maximum number of attempts to read KBC output)
-*/
-int (kbc_read_output)(uint8_t *output);
+ */
+int(kbc_read_output)(uint8_t *output);
 
 /**
  * @brief Reads the KBC acknowledgment byte from the output buffer
- * 
+ *
  * @param acknowledgment_byte address of memory position to be filled with the KBC acknowledgment byte
  * @return Return 0 upon success and non-zero otherwise
-*/
-int (kbc_read_acknowledgment_byte)(uint8_t *acknowledgment_byte);
+ */
+int(kbc_read_acknowledgment_byte)(uint8_t *acknowledgment_byte);
 
 /**
  * @brief Writes command to KBC
- * 
+ *
  * @param command command to be written to KBC
  * @return Return 0 upon success and non-zero otherwise (1 if internal error, 2 if exceeded maximum number of attempts to write command to KBC)
-*/
-int (kbc_write_command)(uint8_t command);
+ */
+int(kbc_write_command)(uint8_t command);
 
 /**
  * @brief Writes argument to KBC
- * 
+ *
  * @param argument argument to be written to KBC
  * @return Return 0 upon success and non-zero otherwise (1 if internal error, 2 if exceeded maximum number of attempts to write argument to KBC)
-*/
-int (kbc_write_argument)(uint8_t argument);
+ */
+int(kbc_write_argument)(uint8_t argument);
 
 /**@}*/
 
